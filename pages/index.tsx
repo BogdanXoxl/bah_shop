49482560e40217376810f3ec0d94e662a9f7e4a2
bogxoxl <BogdanXoxl>
2022-04-19 17:06:34 +0300

    test hg

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  asd;
   return (
     <div className={styles.container}>
       <Head>
